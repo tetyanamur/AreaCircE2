36ee79435fa826cab7779147db4974c5bc27f5da
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-29 15:18:34 -0500

    initial commit.

---
+++ AreaCircE2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Exercise2
+    {
+        public static double AreaoOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle in this radius is {AreaoOfCircle(radius)} ");
+        }
+    }
+}
